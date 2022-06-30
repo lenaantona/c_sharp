283b4e572330d479e81333c0f0a74223c18914bd
lenaantona <mihaleva_alena@mail.ru>
2022-06-30 20:18:08 +0500

    Задание №13

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+//показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+int rndNum = new Random().Next(100,1000);
+Console.WriteLine(rndNum);
+int NewNum(int num)
+{
+    int num1 = num % 100;
+    int num2 = num % 10;
+    return num = num1 - num2;
+}
+int result = NewNum(rndNum) / 10;
+Console.WriteLine(result);
