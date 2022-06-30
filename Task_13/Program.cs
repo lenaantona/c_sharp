f63abbed5db4de71260e1cb5553401f66c945052
lenaantona <mihaleva_alena@mail.ru>
2022-06-30 20:46:22 +0500

    Задание №15

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+if (num>99)
+{
+   while (index<2)
+   {
+    num = (num - num % 10) / 10;
+    index++;
+   }
+   Console.WriteLine(num);
+}
+else Console.WriteLine("Третьей цифры нет");
