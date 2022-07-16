b53c613cd79771911b397ef44c678adcd1c64083
lenaantona <mihaleva_alena@mail.ru>
2022-07-16 22:17:43 +0500

    задачи 41,43

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+Console.Write("Колличество чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int [] NewArray(int len)
+{
+    int [] arrays = new int [len];
+    for (int i = 0; i < len; i++)  
+    {
+        Console.Write("Введите число: ");
+        arrays[i] = Convert.ToInt32(Console.ReadLine());
+    } 
+    return arrays;
+}
+int CountNumber (int [] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+       
+        if (arr[i] > 0) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int [] ArraysRes = NewArray(M);
+int result = CountNumber(ArraysRes);
+Console.Write($"Кол-во чисел больше 0 = {result}");
--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+void XY (double bb2, double bb1, double kk1, double kk2)
+{
+    double x = (bb2 - bb1)/(kk1 - kk2);
+    double y = kk1 * x + bb1;
+    Console.Write($"({x};{y})");
+}
+XY (b2, b1, k1, k2);
